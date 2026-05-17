5cd9303996a87ed6c042c25315e83c63ae0df0b5
Younes Lahouiti <loller763@gmail.com>
2026-05-17 12:11:07 +0200

    init new util class for the experience to handle sizes

---
+++ src/modules/Experience/utils/Sizes/Sizes.ts
@@ -0,0 +1,47 @@
+class Sizes {
+  width: number = 0;
+  height: number = 0;
+  pixelRatio: number = 1;
+
+  private readonly resizeObserver: ResizeObserver;
+
+  constructor(width: number, height: number) {
+    this.setSize(width, height);
+
+    this.resizeObserver = new ResizeObserver(this.onResize);
+  }
+
+  get aspectRatio() {
+    return this.width / this.height;
+  }
+
+  setSize = (width: number, height: number) => {
+    this.width = width || NaN;
+    this.height = height || NaN;
+
+    this.pixelRatio = Math.min(window.devicePixelRatio, 2);
+  };
+
+  private onResize = (entries: ResizeObserverEntry[]) => {
+    for (const entry of entries) {
+      if (!entry.contentRect) return;
+      const { width, height } = entry.contentRect;
+
+      this.setSize(width, height);
+    }
+  };
+
+  beginObserve = (canvas: HTMLCanvasElement) => {
+    this.resizeObserver.observe(canvas);
+  };
+
+  endObserve = (canvas: HTMLCanvasElement) => {
+    this.resizeObserver.unobserve(canvas);
+  };
+
+  destroy = () => {
+    this.resizeObserver.disconnect();
+  };
+}
+
+export default Sizes;
