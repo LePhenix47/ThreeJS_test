b1ac437d965078ae7aad468999e55d64d19f81ac
Younes Lahouiti <loller763@gmail.com>
2026-05-31 19:38:00 +0200

    Added lil-gui params for the env for the env instnaisty + sunlight intensity

--- src/modules/World/Environment.ts
+++ src/modules/World/Environment.ts
@@ -1,5 +1,6 @@
-import Experience from "@modules/Experience/Experience";
+import Experience, { Destroyable } from "@modules/Experience/Experience";
 import * as THREE from "three";
+import GUIStateRegistry from "@/utils/classes/gui-state-registry";
 
 type EnvironmentMap = {
   texture: THREE.CubeTexture | null;
@@ -7,10 +8,14 @@ type EnvironmentMap = {
   updateMaterial: () => void;
 };
 
-class Environment {
+class Environment implements Destroyable {
   private readonly experience: Experience | null;
   private sunLight: THREE.DirectionalLight;
   private envMap: EnvironmentMap;
+  private guiRegistry: GUIStateRegistry<{
+    envMapIntensity: number;
+    sunLightIntensity: number;
+  }> | null = null;
 
   private get scene() {
     return this.experience!.scene;
@@ -20,21 +25,25 @@ class Environment {
     return this.experience!.resources;
   }
 
+  private get debug() {
+    return this.experience!.debug;
+  }
+
   constructor() {
     console.log("World");
 
     this.experience = Experience.instance;
     if (!this.experience) throw new Error("Experience instance not found");
 
-    // * Env map
     this.envMap = this.setEnvMap();
-
-    // * Sunlight
     this.sunLight = this.initSunLight(true);
     this.scene.add(this.sunLight);
-
     this.scene.environment = this.envMap.texture;
     this.scene.background = this.envMap.texture;
+
+    if (this.debug.isActive) {
+      this.addDebugFolders();
+    }
   }
 
   private setEnvMap = (): EnvironmentMap => {
@@ -84,9 +93,35 @@ class Environment {
       child.material.needsUpdate = true;
     });
   };
-  /*
+  private addDebugFolders = () => {
+    const registry = new GUIStateRegistry("environment-gui-state", {
+      envMapIntensity: this.envMap.intensity,
+      sunLightIntensity: this.sunLight.intensity,
+    });
 
-  */
+    registry
+      .bind("envMapIntensity", (v) => {
+        this.envMap.intensity = v;
+        this.envMap.updateMaterial();
+      })
+      .bind("sunLightIntensity", (v) => {
+        this.sunLight.intensity = v;
+      });
+
+    this.guiRegistry = registry;
+
+    const folder = this.debug.gui.addFolder("Environment");
+    folder
+      .add(registry.state, "envMapIntensity", 0, 4, 0.001)
+      .name("Env Map Intensity");
+    folder
+      .add(registry.state, "sunLightIntensity", 0, 10, 0.001)
+      .name("Sun Light Intensity");
+  };
+
+  public destroy = () => {
+    this.guiRegistry?.dispose();
+  };
 }
 
 export default Environment;
