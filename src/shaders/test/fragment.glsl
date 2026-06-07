24ee10a1c6e7ad7bf24ebae30c4f3525a41cccd7
Younes Lahouiti <loller763@gmail.com>
2026-06-07 19:17:05 +0200

    refactored the vertex shader code a bit + added an elevation varying to simulate shadows on the lower elevations of the flag

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -6,8 +6,12 @@ uniform sampler2D uTexture; // * We need sampler2D type for imported THREE.js te
 varying float vRandom;
 varying vec2 vUv;
 
+varying float vElevation;
+
 void main() {
     vec4 textureColor = texture2D(uTexture, vUv);
+
+    textureColor.rgb *= vElevation * 2.0 + 0.5;
 
     gl_FragColor = textureColor;
 }
--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -9,22 +9,30 @@ attribute float aRandom;
 attribute vec3 position;
 attribute vec2 uv;
 
+// * Varying from THREE
 varying float vRandom;
 varying vec2 vUv;
+
+// * Local varying vars
+varying float vElevation;
 
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
+// * Non-varying Values to send to the fragment
+    float elevation = sin(modelPosition.x * uFrequency.x - uTime) * 0.1;
+    elevation += sin(modelPosition.y * uFrequency.y - uTime) * 0.1;
+    modelPosition.z += elevation;
+
     // * Creates wave effect for the flag
-    modelPosition.z += sin(modelPosition.x * uFrequency.x + uTime) * 0.1;
-    modelPosition.z += sin(modelPosition.y * uFrequency.y + uTime) * 0.1;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
 
+// ? Varying value instantiation
     vRandom = aRandom;
-
     vUv = uv;
+    vElevation = elevation;
 }
