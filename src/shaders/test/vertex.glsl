674fa99a8ec4586a7891c9f22e0408198bab214f
Younes Lahouiti <loller763@gmail.com>
2026-06-07 15:24:50 +0200

    Made the plane wavy with the vertex shader (cool shit we can't do just with Three.js)

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -9,6 +9,8 @@ void main() {
     // Option A — Three.js built-in (modelViewMatrix = viewMatrix * modelMatrix)
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
+    modelPosition.z += sin(modelPosition.x * 10.0) * 0.1;
+
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
