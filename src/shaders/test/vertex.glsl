2280d1eed35f437c5591e2afcbcd322ea3a08d08
Younes Lahouiti <loller763@gmail.com>
2026-06-07 15:22:55 +0200

    Updated vertex shader so we can easily change more stuff

--- src/shaders/test/vertex.glsl
+++ src/shaders/test/vertex.glsl
@@ -1,12 +1,18 @@
 uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
+uniform mat4 modelMatrix;
 uniform mat4 viewMatrix;
 
 attribute vec3 position;
 
 void main() {
     // Option A — Three.js built-in (modelViewMatrix = viewMatrix * modelMatrix)
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
 
     // Option B — explicit split (same result, modelViewMatrix NOT used)
     // gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
