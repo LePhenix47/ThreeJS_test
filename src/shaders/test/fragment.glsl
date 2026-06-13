ddbbad068960151c27a90c0479c00d637a297f38
Younes Lahouiti <loller763@gmail.com>
2026-06-13 12:54:37 +0200

    minor refactor for pattern 3

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -5,6 +5,6 @@ varying vec2 vUv;
 
 void main() {
 
-    gl_FragColor = vec4(vUv.x, vUv.x, vUv.x, 1.0);
-    // gl_FragColor = vec4(vPos, 1.0);
+    float strength = vUv.x;
+    gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
