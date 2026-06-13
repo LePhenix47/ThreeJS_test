c07056016ad74491950a2f09db154079f025a803
Younes Lahouiti <loller763@gmail.com>
2026-06-13 20:25:42 +0200

    pattern 16

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -4,13 +4,8 @@ varying vec3 vPos;
 varying vec2 vUv;
 
 void main() {
-    float strengthX = step(0.4, mod(vUv.x * 10.0, 1.0));
-    strengthX *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
 
-    float strengthY = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
-    strengthY *= step(0.4, mod(vUv.y * 10.0, 1.0));
-
-    float strength = strengthX + strengthY;
+    float strength = abs(vUv.r - 0.5);
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
