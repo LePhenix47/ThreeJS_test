28bbf01451a8b8b0d31fc1825f33444c30b0f8ee
Younes Lahouiti <loller763@gmail.com>
2026-06-13 20:19:54 +0200

    pattern 15

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -5,9 +5,9 @@ varying vec2 vUv;
 
 void main() {
     float strengthX = step(0.4, mod(vUv.x * 10.0, 1.0));
-    strengthX *= step(0.8, mod(vUv.y * 10.0, 1.0));
+    strengthX *= step(0.8, mod(vUv.y * 10.0 + 0.2, 1.0));
 
-    float strengthY = step(0.8, mod(vUv.x * 10.0, 1.0));
+    float strengthY = step(0.8, mod(vUv.x * 10.0 + 0.2, 1.0));
     strengthY *= step(0.4, mod(vUv.y * 10.0, 1.0));
 
     float strength = strengthX + strengthY;
