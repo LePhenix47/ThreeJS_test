a223b186382829f7264bf1813f3563c8cb9dfc8f
Younes Lahouiti <loller763@gmail.com>
2026-06-14 13:35:30 +0200

    pattern 30

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -4,8 +4,21 @@ varying vec3 vPos;
 varying vec2 vUv;
 
 void main() {
-    // float strength = length(vUv - 0.5);
-    float strength = 1.5 * 10e-3 / distance(vUv, vec2(0.5)); // 0.015 / distance from center
+
+    float stretchX = 0.1;
+    float stretchXOffset = 0.5 - stretchX / 2.0;
+
+    float stretchY = 0.5;
+    float stretchYOffset = 0.5 - stretchY / 2.0;
+
+    vec2 lightUv = vec2(
+        // 
+    vUv.x * stretchX + stretchXOffset,
+        // 
+    vUv.y * stretchY + stretchYOffset
+    // 
+    );
+    float strength = 1.5 * 10e-3 / distance(lightUv, vec2(0.5)); // 0.015 / distance from center
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
