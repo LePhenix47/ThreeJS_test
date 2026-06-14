c11e5c5b057a8a55b7eb840f03d331edf8457fee
Younes Lahouiti <loller763@gmail.com>
2026-06-14 18:24:31 +0200

    pattern 49

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -74,7 +74,7 @@ float cnoise(vec2 P) {
 
 void main() {
 
-    float strength = 1.0 - abs(cnoise(vUv * 10.0));
+    float strength = sin(cnoise(vUv * 10.0) * 20.0);
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
