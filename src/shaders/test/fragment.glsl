320a2c8298a6314ef5e4d2883a52e3afca20cb95
Younes Lahouiti <loller763@gmail.com>
2026-06-14 17:57:56 +0200

    pattern 42

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -3,6 +3,8 @@ precision mediump float; // ? Medium precision float (~10-bit mantissa, minimum 
 varying vec3 vPos;
 varying vec2 vUv;
 
+#define PI 3.1415926535897932384626433832795
+
 vec2 rotationMatrix(vec2 coords, float angleDeg, vec2 origin) {
     float angleRad = radians(angleDeg);
 
@@ -20,6 +22,9 @@ vec2 rotationMatrix(vec2 coords, float angleDeg, vec2 origin) {
 
 void main() {
     float angle = atan(vUv.x - 0.5, vUv.y - 0.5);
+
+    angle /= PI * 2.0;
+    angle += 0.5;
 
     float strength = angle;
 
