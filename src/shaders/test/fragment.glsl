56fb7ea3de08974e6aecb3e39287340cd9629e01
Younes Lahouiti <loller763@gmail.com>
2026-06-14 17:11:47 +0200

    pattern 32

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -3,10 +3,34 @@ precision mediump float; // ? Medium precision float (~10-bit mantissa, minimum 
 varying vec3 vPos;
 varying vec2 vUv;
 
+vec2 rotationMatrix(vec2 coords, float angleDeg, vec2 origin) {
+    float angleRad = radians(angleDeg);
+
+    float cosAngle = cos(angleRad);
+    float sinAngle = sin(angleRad);
+
+    float dx = (coords.x - origin.x);
+    float dy = (coords.y - origin.y);
+
+    float rotationX = dx * cosAngle - dy * sinAngle + origin.x;
+    float rotationY = dy * cosAngle + dx * sinAngle + origin.y;
+
+    return vec2(rotationX, rotationY);
+}
+
 void main() {
 
-    float strength = 0.15 / (distance(vec2(vUv.x, (vUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
-    strength *= 0.15 / (distance(vec2(vUv.y, (vUv.x - 0.5) * 5.0 + 0.5), vec2(0.5))); // 0.015 / distance from center
+    float angleDeg = radians(45.0);
+
+    vec2 rotatedUv = vUv;
+
+    vec2 testUv = rotationMatrix(vUv, 45.0, vec2(0.5));
+
+    rotatedUv.x = testUv.x;
+    rotatedUv.y = testUv.y;
+
+    float strength = 0.15 / (distance(vec2(rotatedUv.x, (rotatedUv.y - 0.5) * 5.0 + 0.5), vec2(0.5)));
+    strength *= 0.15 / (distance(vec2(rotatedUv.y, (rotatedUv.x - 0.5) * 5.0 + 0.5), vec2(0.5))); // 0.015 / distance from center
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
