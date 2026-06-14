1bee6d1a80081cdfe3c0db3da3812970c58f85ee
Younes Lahouiti <loller763@gmail.com>
2026-06-14 13:07:27 +0200

    pattern 28

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -5,7 +5,7 @@ varying vec2 vUv;
 
 void main() {
     // float strength = length(vUv - 0.5);
-    float strength = distance(vUv, vec2(0.5));
+    float strength = 1.0 - distance(vUv, vec2(0.5));
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
