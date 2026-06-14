6e1594ffc8bc6ee9d6336d382c5883e52b353056
Younes Lahouiti <loller763@gmail.com>
2026-06-14 13:04:33 +0200

    pattern 26

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -3,27 +3,9 @@ precision mediump float; // ? Medium precision float (~10-bit mantissa, minimum 
 varying vec3 vPos;
 varying vec2 vUv;
 
-// Source - https://stackoverflow.com/a/4275343
-// Posted by appas, modified by community. See post 'Timeline' for change history
-// Retrieved 2026-06-14, License - CC BY-SA 4.0
-
-float random(vec2 co) {
-    return fract(sin(dot(co, vec2(12.9898, 78.233))) * 43758.5453);
-}
-
-float floorFloat(float value, float maxFractionDigits) {
-
-    float powerOfTen = pow(10.0, maxFractionDigits);
-    float flooredVal = floor(value * powerOfTen) / powerOfTen;
-    return flooredVal;
-}
-
 void main() {
-    float floorX = floorFloat(vUv.x, 1.0);
-    float floorY = floorFloat((vUv.y + vUv.x), 1.0);
-
-    vec2 gridUv = vec2(floorX, floorY);
-    float strength = random(gridUv);
+    // float strength = distance(vUv, vec2(0.0));
+    float strength = length(vUv);
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
