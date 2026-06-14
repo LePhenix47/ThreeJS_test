c354074081cacaded5a2acf77dcd4e471aae9bea
Younes Lahouiti <loller763@gmail.com>
2026-06-14 17:43:20 +0200

    pattern 40

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -19,10 +19,9 @@ vec2 rotationMatrix(vec2 coords, float angleDeg, vec2 origin) {
 }
 
 void main() {
+    float angle = atan(vUv.x, vUv.y);
 
-    vec2 wavedUv = vec2(vUv.x + sin(vUv.y * 100.0) * 0.1, vUv.y + sin(vUv.x * 100.0) * 0.1);
-
-    float strength = 1.0 - step(0.01, abs(distance(wavedUv, vec2(0.5)) - 0.25));
+    float strength = angle;
 
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
