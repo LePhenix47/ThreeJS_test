a4ec0251685c0ef63eb5ccf6f8efc5876b08353d
Younes Lahouiti <loller763@gmail.com>
2026-06-21 11:39:56 +0200

    removed useless comments from vertex shader

--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -1,11 +1,8 @@
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-// * Non-varying Values to send to the fragment
-
-    // * Creates wave effect for the flag
-
     vec4 viewPosition = viewMatrix * modelPosition;
+
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
