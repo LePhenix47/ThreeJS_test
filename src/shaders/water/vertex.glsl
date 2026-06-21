a2f9c5619f6a5a1fbc66f0eb4d9409a6db4484fd
Younes Lahouiti <loller763@gmail.com>
2026-06-21 13:17:24 +0200

    added a varying vElevation to pass onto the frag shader

--- src/shaders/water/vertex.glsl
+++ src/shaders/water/vertex.glsl
@@ -3,14 +3,20 @@ uniform float uTimePlayBackSpeed;
 uniform float uWavesElevation;
 uniform vec2 uWavesFrequency;
 
+varying float vElevation;
+
 void main() {
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-    float elevation = uWavesElevation * sin(modelPosition.x * uWavesFrequency.x + uTime * uTimePlayBackSpeed);
+    float timePlayBack = uTime * uTimePlayBackSpeed;
+    float elevation = uWavesElevation * sin(modelPosition.x * uWavesFrequency.x + timePlayBack) * sin(modelPosition.z * uWavesFrequency.y + timePlayBack);
     modelPosition.y += elevation;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
+
+    // * Setting varying values
+    vElevation = elevation;
 }
