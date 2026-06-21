6e3e7026573f80faefea946b740738e060310540
Younes Lahouiti <loller763@gmail.com>
2026-06-21 13:17:38 +0200

    Imported unforms into frag shader

--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -1,7 +1,11 @@
 precision mediump float; // ? Medium precision float (~10-bit mantissa, minimum range ±2^14)
 
 // uniform float uTime;
+uniform vec3 uSurfaceColor;
+uniform vec3 uDepthColor;
+
+varying float vElevation;
 
 void main() {
-    gl_FragColor = vec4(0.5, 0.8, 1.0, 1.0);
+    gl_FragColor = vec4(uSurfaceColor, 1.0);
 }
