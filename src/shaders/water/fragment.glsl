947a3e144b0a8a6687754d06537f12961b0a7c99
Younes Lahouiti <loller763@gmail.com>
2026-06-21 14:33:30 +0200

    added color change based on elevation

--- src/shaders/water/fragment.glsl
+++ src/shaders/water/fragment.glsl
@@ -7,5 +7,10 @@ uniform vec3 uDepthColor;
 varying float vElevation;
 
 void main() {
-    gl_FragColor = vec4(uSurfaceColor, 1.0);
+    /*
+    * It's as if we were making a linear gradient 
+    *and we chose the color based on the offset from the elevation
+    */
+    vec3 color = mix(uDepthColor, uSurfaceColor, vElevation);
+    gl_FragColor = vec4(color, 1.0);
 }
