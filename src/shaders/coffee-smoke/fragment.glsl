085d21eb76ea4648dacc47475bd77963d593df12
Younes Lahouiti <loller763@gmail.com>
2026-07-18 15:52:23 +0200

    following the lesson ¯\_(ツ)_/¯

--- src/shaders/coffee-smoke/fragment.glsl
+++ src/shaders/coffee-smoke/fragment.glsl
@@ -7,14 +7,16 @@ void main() {
   vec2 smokeUv = vUv;
   smokeUv.x *= 0.5;
   smokeUv.y *= 0.3;
-  smokeUv.y -= uTime * 0.02;
+  smokeUv.y -= uTime * 0.03;
 
   vec4 texture = texture(uPerlinNoiseTexture, smokeUv);
 
-  float alpha = texture.r;
-  alpha *= smoothstep(0.0, 0.1, vUv.y);   // * fade in at bottom, 0 → 10%
-  alpha *= smoothstep(1.0, 0.5, vUv.y);   // * fade out at top, 100% → 50%
-  gl_FragColor = vec4(vec3(1.0), alpha);
+  float smoke = texture.r;
+  smoke *= smoothstep(0.04, 1.0, smoke);
+  // smoke *= smoothstep(1.0, 0.9, vUv.x);
+  // smoke *= smoothstep(0.0, 0.1, vUv.y);
+  // smoke *= smoothstep(1.0, 0.4, vUv.y);
+  gl_FragColor = vec4(vec3(1.0), smoke);
 
   #include <tonemapping_fragment>
   #include <colorspace_fragment>
