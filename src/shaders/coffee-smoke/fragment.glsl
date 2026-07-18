98524b19dafc897c4ca11060762585ef60be5013
Younes Lahouiti <loller763@gmail.com>
2026-07-18 15:37:25 +0200

    Scaled and animated the perlin noise on frag

--- src/shaders/coffee-smoke/fragment.glsl
+++ src/shaders/coffee-smoke/fragment.glsl
@@ -1,9 +1,16 @@
 uniform sampler2D uPerlinNoiseTexture;
+uniform float uTime;
+
 varying vec2 vUv;
 
 void main() {
-  vec4 texture = texture(uPerlinNoiseTexture, vUv);
-  gl_FragColor = vec4(vec3(texture), 1.0 - texture.r);
+  vec2 smokeUv = vUv;
+  smokeUv.x *= 0.5;
+  smokeUv.y *= 0.3;
+  smokeUv.y -= uTime * 0.02;
+
+  vec4 texture = texture(uPerlinNoiseTexture, smokeUv);
+  gl_FragColor = vec4(vec3(1.0), texture.r);
 
   #include <tonemapping_fragment>
   #include <colorspace_fragment>
