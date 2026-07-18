acf0ded7c56c673a707b2df604c758c7d18587e7
Younes Lahouiti <loller763@gmail.com>
2026-07-18 16:30:49 +0200

    added randomness to the perlin noise

--- src/shaders/coffee-smoke/vertex.glsl
+++ src/shaders/coffee-smoke/vertex.glsl
@@ -21,7 +21,7 @@ vec2 rotationMatrix(vec2 coords, float angleDeg, vec2 origin) {
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-  vec4 perlinTexture = texture(uPerlinNoiseTexture, uv);
+  vec4 perlinTexture = texture(uPerlinNoiseTexture, uv + vec2(uTime) * 0.1);
   float perlinNoise = perlinTexture.r;
 
   float angle = modelPosition.y * 360.0 / 12.0 + uTime * 20.0 + perlinNoise * 100.0;
