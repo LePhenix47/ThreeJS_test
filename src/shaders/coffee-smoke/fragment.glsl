4ac896567921fcccf47aedd8a36de385bb57ffd4
Younes Lahouiti <loller763@gmail.com>
2026-07-18 15:22:40 +0200

    Updated code to use modern GLSL texture loader

--- src/shaders/coffee-smoke/fragment.glsl
+++ src/shaders/coffee-smoke/fragment.glsl
@@ -2,7 +2,7 @@ uniform sampler2D uPerlinNoiseTexture;
 varying vec2 vUv;
 
 void main() {
-  vec4 texture = texture2D(uPerlinNoiseTexture, vUv);
+  vec4 texture = texture(uPerlinNoiseTexture, vUv);
   gl_FragColor = vec4(vec3(texture), 1.0 - texture.r);
 
   #include <tonemapping_fragment>
