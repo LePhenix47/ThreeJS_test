ce8e8785e2d7fb2fbb237726a42e1bf43dafa9da
Younes Lahouiti <loller763@gmail.com>
2026-07-19 16:45:16 +0200

    Minor refactor for perlin texture sampling + added wind !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- src/shaders/coffee-smoke/vertex.glsl
+++ src/shaders/coffee-smoke/vertex.glsl
@@ -18,17 +18,31 @@ vec2 rotationMatrix(vec2 coords, float angleDeg, vec2 origin) {
   return rotMat * (coords - origin) + origin;
 }
 
+float samplePerlinTexture(float uvOffsetX, float uvOffsetY) {
+  vec4 perlinTexture = texture(uPerlinNoiseTexture, vec2(uvOffsetX, uvOffsetY));
+
+  return perlinTexture.r;
+}
+
 void main() {
   vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
-  vec4 perlinTexture = texture(uPerlinNoiseTexture, vec2(0.5, uv.y - uTime * 0.05));
-  // vec4 perlinTexture = texture(uPerlinNoiseTexture, uv + vec2(uTime) * 0.1);
+// * Smoke
+  float perlinNoiseSmoke = samplePerlinTexture(0.5, uv.y - uTime * 0.05);
 
-  float perlinNoise = perlinTexture.r;
-
-  float angle = modelPosition.y * 360.0 / 12.0 + uTime * 20.0 + perlinNoise * 100.0;
+  float angle = modelPosition.y * 360.0 / 12.0 + uTime * 20.0 + perlinNoiseSmoke * 100.0;
   vec2 rotation = rotationMatrix(modelPosition.xz, angle, vec2(0.0));
   modelPosition.xz = rotation;
+
+  // * Wind
+  float playBackSpeed = uTime * 0.01;
+  float perlinNoiseWindX = samplePerlinTexture(0.25, playBackSpeed) - 0.5;
+  float perlinNoiseWindZ = samplePerlinTexture(0.75, playBackSpeed) - 0.5;
+  vec2 windOffset = vec2(perlinNoiseWindX, perlinNoiseWindZ);
+
+  windOffset *= pow(uv.y, 2.0) * 10.0;
+
+  modelPosition.xz += windOffset;
 
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
