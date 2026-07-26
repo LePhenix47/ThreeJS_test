0ab598cb9731fdbe4829f4769389391857c42b85
Younes Lahouiti <loller763@gmail.com>
2026-07-26 17:01:44 +0200

    increased stripe strength

--- src/shaders/holographic/fragment.glsl
+++ src/shaders/holographic/fragment.glsl
@@ -6,6 +6,8 @@ void main() {
     // float stripes = vRelativeY;
     float stripes = mod((vRelativeY - uTime * 0.1) * 20.0, 1.0);
 
+    stripes = pow(stripes, 3.0);
+
     gl_FragColor = vec4(vec3(1.0), stripes);
 
   #include <tonemapping_fragment>
