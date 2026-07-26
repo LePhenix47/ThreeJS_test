db817a9a3db6ec922315612869f96d8ad940ccd4
Younes Lahouiti <loller763@gmail.com>
2026-07-26 17:01:30 +0200

    Added transparent stripes

--- src/shaders/holographic/fragment.glsl
+++ src/shaders/holographic/fragment.glsl
@@ -1,7 +1,12 @@
-varying vec3 vModelPosition;
+varying float vRelativeY;
+
+uniform float uTime;
 
 void main() {
-    gl_FragColor = vec4(vModelPosition, 1.0);
+    // float stripes = vRelativeY;
+    float stripes = mod((vRelativeY - uTime * 0.1) * 20.0, 1.0);
+
+    gl_FragColor = vec4(vec3(1.0), stripes);
 
   #include <tonemapping_fragment>
   #include <colorspace_fragment>
