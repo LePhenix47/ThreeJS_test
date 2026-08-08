d68755008e585d48dc3c7914479430a0d06b45ee
Younes Lahouiti <loller763@gmail.com>
2026-08-08 17:24:25 +0200

    Improved perfs

--- src/shaders/fireworks/fragment.glsl
+++ src/shaders/fireworks/fragment.glsl
@@ -4,9 +4,11 @@ void main() {
 
   vec2 uv = gl_PointCoord;
   // uv.y = 1.0 - uv.y; // ? if on three JS we haven't set the flipY property
-  vec4 textureColor = texture(uTexture, uv);
 
-  gl_FragColor = vec4(textureColor);
+  // * All the textures are gray-scaled so R=G=B, and we can just focus on the alpha, better for performance 
+  float textureAlphaColor = texture(uTexture, uv).r;
+
+  gl_FragColor = vec4(vec3(1.0), textureAlphaColor);
 
   #include <tonemapping_fragment>
   #include <colorspace_fragment>
