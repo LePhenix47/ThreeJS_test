c4eb14cf318dbe75fa4ebbf7964ebfee6f3510f1
Younes Lahouiti <loller763@gmail.com>
2026-08-08 17:21:00 +0200

    rename

--- src/shaders/fireworks/fragment.glsl
+++ src/shaders/fireworks/fragment.glsl
@@ -4,9 +4,9 @@ void main() {
 
   vec2 uv = gl_PointCoord;
   // uv.y = 1.0 - uv.y; // ? if on three JS we haven't set the flipY property
-  vec4 color = texture(uTexture, uv);
+  vec4 textureColor = texture(uTexture, uv);
 
-  gl_FragColor = vec4(color);
+  gl_FragColor = vec4(textureColor);
 
   #include <tonemapping_fragment>
   #include <colorspace_fragment>
