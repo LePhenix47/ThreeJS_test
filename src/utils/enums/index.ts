0c88fc170d8755974016f6ae023a242ad6ea454e
Younes Lahouiti <loller763@gmail.com>
2026-08-08 13:43:16 +0200

    Added new method to Enum static class to get the length of an enum

--- src/utils/enums/index.ts
+++ src/utils/enums/index.ts
@@ -1,57 +1,54 @@
+type EnumType = Record<string, string | number>;
+
 export default class Enum {
   /**
    * Returns all keys (names) of the enum
    */
-  static keys<T extends Record<string, string | number>>(
-    enumObj: T,
-  ): (keyof T)[] {
+  static keys<T extends EnumType>(enumObj: T): (keyof T)[] {
     return Object.keys(enumObj).filter((key) =>
       isNaN(Number(key)),
     ) as (keyof T)[];
   }
 
   /**
+   * Returns the length of the enum (how many keys it has)
+   */
+  static length<T extends EnumType>(enumObj: T): number {
+    return this.keys(enumObj).length;
+  }
+
+  /**
    * Returns all values of the enum (numeric or string)
    */
-  static values<T extends Record<string, string | number>>(
-    enumObj: T,
-  ): T[keyof T][] {
+  static values<T extends EnumType>(enumObj: T): T[keyof T][] {
     return Enum.keys(enumObj).map((key) => enumObj[key]);
   }
 
   /**
    * Returns key-value pairs as [key, value][]
    */
-  static entries<T extends Record<string, string | number>>(
-    enumObj: T,
-  ): Array<[keyof T, T[keyof T]]> {
+  static entries<T extends EnumType>(enumObj: T): Array<[keyof T, T[keyof T]]> {
     return Enum.keys(enumObj).map((key) => [key, enumObj[key]]);
   }
 
   /**
    * Checks if a key exists in the enum
    */
-  static has<T extends Record<string, string | number>>(
-    enumObj: T,
-    key: string,
-  ): boolean {
+  static has<T extends EnumType>(enumObj: T, key: string): boolean {
     return Enum.keys(enumObj).includes(key as keyof T);
   }
 
   /**
    * Checks if a value exists in the enum
    */
-  static hasValue<T extends Record<string, string | number>>(
-    enumObj: T,
-    value: T[keyof T],
-  ): boolean {
+  static hasValue<T extends EnumType>(enumObj: T, value: T[keyof T]): boolean {
     return Enum.values(enumObj).includes(value);
   }
 
   /**
    * Gets the key/name for a given enum value
    */
-  static getName<T extends Record<string, string | number>>(
+  static getName<T extends EnumType>(
     enumObj: T,
     value: T[keyof T],
   ): keyof T | undefined {
