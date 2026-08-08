05ee4efba2e5a16658a09e8f200e9cd7e4e8631d
Younes Lahouiti <loller763@gmail.com>
2026-08-08 13:16:21 +0200

    Removal of the Floor

--- src/modules/World/World.ts
+++ src/modules/World/World.ts
@@ -3,7 +3,6 @@ import Experience, {
   Updatable,
 } from "@modules/Experience/Experience";
 import Environment from "./Environment";
-import Floor from "./Floor";
 import * as THREE from "three";
 import GUIStateRegistry from "@/utils/classes/gui-state-registry";
 
@@ -17,7 +16,6 @@ type WorldState = {
 class World implements Updatable, Destroyable {
   private readonly experience: Experience | null;
   public environment?: Environment;
-  public floor?: Floor;
   private axisHelper: THREE.AxesHelper;
   private gridHelper: THREE.GridHelper;
   private guiRegistry: GUIStateRegistry<WorldState> | null = null;
@@ -43,7 +41,6 @@ class World implements Updatable, Destroyable {
     this.experience = Experience.instance;
     if (!this.experience) throw new Error("Experience instance not found");
 
-    this.floor = new Floor();
     this.environment = new Environment();
     this.setHelpers();
 
@@ -109,7 +106,6 @@ class World implements Updatable, Destroyable {
   public update = () => {};
 
   public destroy = () => {
-    this.floor?.destroy();
     this.environment?.destroy();
     this.removeHelpers();
   };
