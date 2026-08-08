92cc0f25515f3bb43b5c4863ea9093d8374a9167
Younes Lahouiti <loller763@gmail.com>
2026-08-08 13:09:36 +0200

    Addition of the GUI params for the sky box

--- src/modules/World/Environment.ts
+++ src/modules/World/Environment.ts
@@ -7,6 +7,13 @@ import { Sky } from "three/addons/objects/Sky.js";
 type EnvironmentState = {
   lightHelper: boolean;
   environmentColor: string;
+  skyTurbidity: number;
+  skyRayleigh: number;
+  skyMieDirectionalG: number;
+  skyMieCoefficient: number;
+  sunPositionX: number;
+  sunPositionY: number;
+  sunPositionZ: number;
 };
 
 class Environment implements Destroyable {
@@ -24,6 +31,13 @@ class Environment implements Destroyable {
   private readonly debugDefaults: EnvironmentState = {
     lightHelper: true,
     environmentColor: "black",
+    skyTurbidity: 10,
+    skyRayleigh: 3,
+    skyMieDirectionalG: 0.95,
+    skyMieCoefficient: 0.1,
+    sunPositionX: 0.3,
+    sunPositionY: -0.038,
+    sunPositionZ: -0.95,
   };
 
   protected envMapTexture: THREE.Texture | THREE.CubeTexture | null = null;
@@ -137,6 +151,45 @@ class Environment implements Destroyable {
     helpersFolder.add(state, "lightHelper").name("Light Helper");
     registry.bind("lightHelper", (v) => {
       this.lightHelper.visible = v;
+    });
+
+    const skyFolder = environMentFolder.addFolder("Sky");
+
+    skyFolder.add(state, "skyTurbidity").min(0).max(20).step(0.1).name("Turbidity");
+    registry.bind("skyTurbidity", (v) => {
+      this.sky.material.uniforms.turbidity.value = v;
+    });
+
+    skyFolder.add(state, "skyRayleigh").min(0).max(10).step(0.01).name("Rayleigh");
+    registry.bind("skyRayleigh", (v) => {
+      this.sky.material.uniforms.rayleigh.value = v;
+    });
+
+    skyFolder.add(state, "skyMieDirectionalG").min(0).max(1).step(0.001).name("Mie Directional G");
+    registry.bind("skyMieDirectionalG", (v) => {
+      this.sky.material.uniforms.mieDirectionalG.value = v;
+    });
+
+    skyFolder.add(state, "skyMieCoefficient").min(0).max(1).step(0.001).name("Mie Coefficient");
+    registry.bind("skyMieCoefficient", (v) => {
+      this.sky.material.uniforms.mieCoefficient.value = v;
+    });
+
+    const sunPosFolder = skyFolder.addFolder("Sun Position");
+
+    sunPosFolder.add(state, "sunPositionX").min(-1).max(1).step(0.001).name("X");
+    registry.bind("sunPositionX", (v) => {
+      this.sky.material.uniforms.sunPosition.value.x = v;
+    });
+
+    sunPosFolder.add(state, "sunPositionY").min(-1).max(1).step(0.001).name("Y");
+    registry.bind("sunPositionY", (v) => {
+      this.sky.material.uniforms.sunPosition.value.y = v;
+    });
+
+    sunPosFolder.add(state, "sunPositionZ").min(-1).max(1).step(0.001).name("Z");
+    registry.bind("sunPositionZ", (v) => {
+      this.sky.material.uniforms.sunPosition.value.z = v;
     });
   };
 
