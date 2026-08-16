c122d35025e4d0f196dd265ac0b7db45cd8e8403
Younes Lahouiti <loller763@gmail.com>
2026-08-16 17:29:27 +0200

    minor fix with the normal thing going below 0

--- src/shaders/utils/lights/directionalLight.glsl
+++ src/shaders/utils/lights/directionalLight.glsl
@@ -4,6 +4,7 @@ vec3 directionalLight(vec3 color, float intensity, vec3 normal, vec3 position) {
     normal = normalize(normal);
 
     float shading = dot(normal, direction);
+    shading = max(0.0, shading);
 
     return color * intensity * shading;
 }
