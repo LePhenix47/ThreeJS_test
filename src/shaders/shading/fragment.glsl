f536aeb80dc75cae0a229004e4e22b0bdd91481f
Younes Lahouiti <loller763@gmail.com>
2026-08-16 13:50:24 +0200

    init addition of the ambient light

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,7 +1,18 @@
 uniform vec3 uColor;
+uniform vec3 uAmbientLightColor;
+uniform float uAmbientLightIntensity;
+
+vec3 ambientLight(vec3 color, float intensity) {
+    return color * intensity;
+}
 
 void main() {
     vec3 color = uColor;
+
+    vec3 light = uAmbientLightColor;
+    light += ambientLight(color, uAmbientLightIntensity);
+
+    color *= light;
 
     gl_FragColor = vec4(color, 1.0);
     #include <tonemapping_fragment>
