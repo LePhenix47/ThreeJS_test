9e6c47dc1930e64e4361128d592b137e240cfbbc
Younes Lahouiti <loller763@gmail.com>
2026-08-30 14:21:27 +0200

    Added GUI params to change X,Y & Z positions + config for the world

--- src/modules/World/World.ts
+++ src/modules/World/World.ts
@@ -9,9 +9,24 @@ import GUIStateRegistry from "@/utils/classes/gui-state-registry";
 type WorldState = {
   axisHelper: boolean;
   gridHelper: boolean;
+  helpersPosX: number;
+  helpersPosY: number;
+  helpersPosZ: number;
 };
 
 class World implements Updatable, Destroyable {
+  public static readonly CONFIG = {
+    axisHelper: {
+      size: 3,
+      yShift: 0.02, // ? To avoid z fighting
+    },
+    gridHelper: {
+      size: 10,
+      subdivisions: 10,
+      yShift: 0.01, // ? To avoid z fighting
+    },
+  };
+
   private readonly experience: Experience | null;
   public ragingSea: RagingSea;
   private axisHelper: THREE.AxesHelper;
@@ -21,6 +36,9 @@ class World implements Updatable, Destroyable {
   private readonly debugDefaults: WorldState = {
     axisHelper: true,
     gridHelper: true,
+    helpersPosX: 0,
+    helpersPosY: 0,
+    helpersPosZ: 0,
   };
 
   private get scene() {
@@ -49,20 +67,68 @@ class World implements Updatable, Destroyable {
     console.log("World");
   }
 
-  private setHelpers = () => {
-    const axisHelper = new THREE.AxesHelper(3);
-    axisHelper.position.y = 0.02;
+  private setAxisHelper(): void {
+    const { size } = World.CONFIG.axisHelper;
+    const axisHelper = new THREE.AxesHelper(size);
 
     this.axisHelper = axisHelper;
+  }
 
-    const gridHelper = new THREE.GridHelper(10, 10);
-    gridHelper.position.y = 0.01;
+  private setGridHelper() {
+    const { size, subdivisions } = World.CONFIG.gridHelper;
+    const gridHelper = new THREE.GridHelper(size, subdivisions);
+
     this.gridHelper = gridHelper;
+  }
+
+  private updateHelpersPositions = (): void => {
+    const {
+      helpersPosX: x,
+      helpersPosY: y,
+      helpersPosZ: z,
+    } = this.guiRegistry?.state || this.debugDefaults;
+
+    const { axisHelper, gridHelper } = World.CONFIG;
+
+    const position = new THREE.Vector3(x, y, z);
+
+    this.updateHelperPosition("axis", position, axisHelper.yShift);
+    this.updateHelperPosition("grid", position, gridHelper.yShift);
+  };
+
+  private updateHelperPosition = (
+    helperType: "axis" | "grid",
+    position: THREE.Vector3,
+    offset: number = 0,
+  ): void => {
+    const newPosition: THREE.Vector3 = structuredClone(position);
+    newPosition.y += offset;
+
+    switch (helperType) {
+      case "axis": {
+        this.axisHelper.position.copy(newPosition);
+        break;
+      }
+      case "grid": {
+        this.gridHelper.position.copy(newPosition);
+        break;
+      }
+
+      default:
+        break;
+    }
+  };
+
+  private setHelpers = (): void => {
+    this.setAxisHelper();
+    this.setGridHelper();
+
+    this.updateHelpersPositions();
 
     this.scene.add(this.axisHelper, this.gridHelper);
   };
 
-  private addDebugFolders = () => {
+  private addDebugFolders(): void {
     const registry = new GUIStateRegistry<WorldState>(
       "world-gui-state",
       this.debugDefaults,
@@ -86,6 +152,30 @@ class World implements Updatable, Destroyable {
       this.gridHelper.visible = v;
     });
 
+    helpersFolder
+      .add(state, "helpersPosX")
+      .min(-5)
+      .max(5)
+      .step(0.01)
+      .name("Helpers X");
+    registry.bind("helpersPosX", this.updateHelpersPositions);
+
+    helpersFolder
+      .add(state, "helpersPosY")
+      .min(-5)
+      .max(5)
+      .step(0.01)
+      .name("Helpers Y");
+    registry.bind("helpersPosY", this.updateHelpersPositions);
+
+    helpersFolder
+      .add(state, "helpersPosZ")
+      .min(-5)
+      .max(5)
+      .step(0.01)
+      .name("Helpers Z");
+    registry.bind("helpersPosZ", this.updateHelpersPositions);
+
     worldFolder
       .add(
         {
@@ -98,7 +188,7 @@ class World implements Updatable, Destroyable {
         "resetPivot",
       )
       .name("Reset Camera Pivot");
-  };
+  }
 
   private removeHelpers = () => {
     this.scene.remove(this.axisHelper, this.gridHelper);
