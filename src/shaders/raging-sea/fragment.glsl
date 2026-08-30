c79d38db9b88eaaa416210aa703d47d9399f600b
Younes Lahouiti <loller763@gmail.com>
2026-08-30 17:26:05 +0200

    Added point light

--- src/shaders/raging-sea/fragment.glsl
+++ src/shaders/raging-sea/fragment.glsl
@@ -19,6 +19,7 @@ void main() {
     // * Light
     vec3 light = vec3(0.0);
     light += directionalLight(vec3(1.0), 1.0, normal, vec3(-1.0, 0.5, 0.0), directionOfView, 30.0);
+    light += pointLight(vec3(1.0), 10.0, normal, vec3(0.0, 0.25, 0.0), directionOfView, 30.0, vPosition, 0.95);
 
     float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
 
