b7acb5fe052a8982eea98e8ba4a4108454ef8f0d
Younes Lahouiti <loller763@gmail.com>
2026-08-30 12:26:56 +0200

    Added a directional light

--- src/shaders/raging-sea/fragment.glsl
+++ src/shaders/raging-sea/fragment.glsl
@@ -7,16 +7,25 @@ varying float vElevation;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
+#include ../utils/lights/directionalLight
 #include ../utils/lights/pointLight
+#include ../utils/vectors/delta
+#include ../utils/vectors/direction
 
 void main() {
     vec3 normal = normalize(vNormal);
+    vec3 directionOfView = direction(cameraPosition, vPosition);
+
+    // * Light
+    vec3 light = vec3(0.0);
+    light += directionalLight(vec3(1.0), 1.0, normal, vec3(-1.0, 0.5, 0.0), directionOfView, 30.0);
 
     float mixStrength = (vElevation + uColorOffset) * uColorMultiplier;
 
     mixStrength = smoothstep(0.0, 1.0, mixStrength);
 
     vec3 color = mix(uDepthColor, uSurfaceColor, mixStrength);
+    color *= light;
 
     gl_FragColor = vec4(color, 1.0);
     #include <colorspace_fragment>
