4f8a169935ca390156ca61a62fb1babe0ea0d4a2
Younes Lahouiti <loller763@gmail.com>
2026-08-30 13:40:59 +0200

    Refactored the wave elevation logic into its own function

--- src/shaders/raging-sea/vertex.glsl
+++ src/shaders/raging-sea/vertex.glsl
@@ -14,19 +14,24 @@ varying vec3 vPosition;
 
 #include ../utils/perlin-noise/perlinClassic3D
 
+float waveElevation(vec3 position) {
+    float elevation = sin(position.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
+        sin(position.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
+        uBigWavesElevation;
+
+    for(float i = 1.0; i <= uSmallIterations; i++) {
+        float noise = perlinClassic3D(vec3(position.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i;
+        elevation -= abs(noise);
+    }
+    return elevation;
+}
+
 void main() {
     // * Base position
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
 
     // Elevation
-    float elevation = sin(modelPosition.x * uBigWavesFrequency.x + uTime * uBigWavesSpeed) *
-        sin(modelPosition.z * uBigWavesFrequency.y + uTime * uBigWavesSpeed) *
-        uBigWavesElevation;
-
-    for(float i = 1.0; i <= uSmallIterations; i++) {
-        float noise = perlinClassic3D(vec3(modelPosition.xz * uSmallWavesFrequency * i, uTime * uSmallWavesSpeed)) * uSmallWavesElevation / i;
-        elevation -= abs(noise);
-    }
+    float elevation = waveElevation(modelPosition.xyz);
 
     modelPosition.y += elevation;
 
