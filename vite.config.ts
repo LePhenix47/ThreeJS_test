a73d89a5bab5ff98a78930cdc72192023fe3f704
Younes Lahouiti <loller763@gmail.com>
2026-09-12 01:46:39 +0200

    Added HTTPS to the localhost server to IP LAN to test on other devices with a secure conte xt to avoid errors WebAPIs requiring HTTPS

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,26 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 import glsl from "vite-plugin-glsl";
+import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
   const env = loadEnv(mode, process.cwd(), "VITE_");
 
   return {
-    plugins: [tanstackRouter(), react(), tsconfigPaths(), glsl()],
+    plugins: [
+      tanstackRouter(),
+      react(),
+      tsconfigPaths(),
+      glsl({
+        removeDuplicatedImports: true,
+      }),
+      mkcert(),
+    ],
     envPrefix: ["VITE_"],
     server: {
       port: 5173, // Change the port to your preferred one
+      strictPort: true, // Fail loudly instead of silently bumping to another port
       host: "0.0.0.0", // Allows access to your local IP address
       open: false, // Optional: Opens the browser automatically
     },
