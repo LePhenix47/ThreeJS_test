dede398fde8c2763b176c2b1374d13cc55262530
Younes Lahouiti <loller763@gmail.com>
2026-09-11 11:28:28 +0200

    Resetted the shader files

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -6,33 +6,7 @@ uniform float uTime;
 uniform vec3 uColor;
 
 void main() {
-    //  * Stripe
-  float stripes = mod((vRelativeY - uTime * 0.1) * 20.0, 1.0);
-
-  stripes = pow(stripes, 3.0);
-
-  // * Fresnel, the reflection of the water surface based on viewing angle, perpendicular = no reflection & transparent, parallel = very reflective & opaque
-  vec3 directionOfView = normalize(vModelPosition - cameraPosition);
-
-  vec3 normal = normalize(vNormal);
-  if(!gl_FrontFacing) {
-    normal *= -1.0;
-  }
-
-  float fresnel = dot(directionOfView, normal) + 1.0;
-  fresnel = pow(fresnel, 2.0);
-
-  // ? Falloff to smooth out edges 
-  float falloff = smoothstep(0.8, 0.0, fresnel); 
-
-  // * Hologram stuff
-  float holographic = fresnel * stripes;
-  holographic += fresnel * 1.25;
-  holographic *= falloff;
-
-  // gl_FragColor = vec4(vec3(1.0), fresnel);
-  // gl_FragColor = vec4(vec3(1.0), stripes);
-  gl_FragColor = vec4(uColor, holographic);
+  gl_FragColor = vec4(0.5);
 
   #include <tonemapping_fragment>
   #include <colorspace_fragment>
--- src/shaders/halftone/vertex.glsl
+++ src/shaders/halftone/vertex.glsl
@@ -1,26 +1,11 @@
-varying vec3 vModelPosition;
-varying vec3 vNormal;
-varying float vRelativeY;
-
 uniform float uTime;
-
-#include ../utils/random2D
 
 void main() {
 
     vec4 modelPosition = modelMatrix * vec4(position, 1.0);
-    modelPosition.x += random2D(modelPosition.xz + uTime) * 0.25;
 
     vec4 viewPosition = viewMatrix * modelPosition;
     vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
-
-    // * Transforms normal from local coord space to world space
-    // ? non-homogenous vec, we do not want to translate nor scale the normals (scale needs normalization so it's fine to keep it)
-    vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
-
-    vModelPosition = modelPosition.xyz;
-    vNormal = modelNormal.xyz;
-    vRelativeY = modelPosition.y - modelMatrix[3].y;
 }
