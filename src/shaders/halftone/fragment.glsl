362bb06d3177b0fb6cb86de8850a9ff7a0ba1ca6
Younes Lahouiti <loller763@gmail.com>
2026-09-11 15:37:20 +0200

    Addition of absolute and relative coords onto the shaders

--- src/shaders/halftone/fragment.glsl
+++ src/shaders/halftone/fragment.glsl
@@ -2,12 +2,13 @@ uniform float uTime;
 uniform vec3 uColor;
 
 varying vec3 vNormal;
-varying vec3 vPosition;
+varying vec3 vRelativePosition; // ? For the halftone
+varying vec3 vAbsolutePosition; // ? For the light
 
 void main() {
   vec3 normal = normalize(vNormal);
 
-  gl_FragColor = vec4(normal, 1.0);
+  gl_FragColor = vec4(vAbsolutePosition, 1.0);
 
   #include <tonemapping_fragment>
   #include <colorspace_fragment>
--- src/shaders/halftone/vertex.glsl
+++ src/shaders/halftone/vertex.glsl
@@ -1,7 +1,8 @@
 uniform float uTime;
 
 varying vec3 vNormal;
-varying vec3 vPosition;
+varying vec3 vRelativePosition;
+varying vec3 vAbsolutePosition;
 
 void main() {
 
@@ -15,5 +16,8 @@ void main() {
     // * varyings
     vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
     vNormal = modelNormal.xyz;
-    vPosition = modelPosition.xyz;
+    vAbsolutePosition = modelPosition.xyz;
+
+    vRelativePosition = vAbsolutePosition;
+    vRelativePosition.y -= modelMatrix[3].y; // ? matrix Y translation
 }
