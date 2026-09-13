c27d9efabf158f8a9d0c3dcc4a397ec6f44392b6
Younes Lahouiti <loller763@gmail.com>
2026-09-13 15:30:12 +0200

    Minor fix on the URL of the OG images and favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -90,7 +90,7 @@ export const Route = createRootRoute({
         property: "og:description",
         content: "Project to learn a thing or two about THREE.js",
       },
-      { property: "og:image", content: "/img/ico/threejs_white.ico" },
+      { property: "og:image", content: `${import.meta.env.BASE_URL}img/ico/threejs_white.ico` },
       { property: "og:image:width", content: "512" },
       { property: "og:image:height", content: "512" },
       {
@@ -104,11 +104,11 @@ export const Route = createRootRoute({
         name: "twitter:description",
         content: "Project to learn a thing or two about THREE.js",
       },
-      { name: "twitter:image", content: "/img/ico/threejs_white.ico" },
+      { name: "twitter:image", content: `${import.meta.env.BASE_URL}img/ico/threejs_white.ico` },
     ],
     links: [
-      { rel: "icon", type: "image/x-icon", media: "(prefers-color-scheme: light)", href: "/img/ico/threejs_black.ico" },
-      { rel: "icon", type: "image/x-icon", media: "(prefers-color-scheme: dark)", href: "/img/ico/threejs_white.ico" },
+      { rel: "icon", type: "image/x-icon", media: "(prefers-color-scheme: light)", href: `${import.meta.env.BASE_URL}img/ico/threejs_black.ico` },
+      { rel: "icon", type: "image/x-icon", media: "(prefers-color-scheme: dark)", href: `${import.meta.env.BASE_URL}img/ico/threejs_white.ico` },
     ],
     scripts: [
       {
