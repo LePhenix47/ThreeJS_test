c2298dc358163eace1c2c08b2e929293c7e8e835
Younes Lahouiti <loller763@gmail.com>
2026-09-18 13:44:22 +0200

    Added varying vec3 vPosition + viewDirection vector helper

--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -1,9 +1,15 @@
+varying vec3 vPosition;
 varying vec3 vNormal;
 varying vec2 vUv;
 
+#include ../utils/vectors/direction
+
 void main() {
-    // gl_FragColor = vec4(vNormal, 1.0);
-    gl_FragColor = vec4(vec3(vUv, 1.0), 1.0);
+    vec3 viewDirection = direction(cameraPosition, vPosition);
+
+    vec3 color = vec3(vUv, 1.0);
+
+    gl_FragColor = vec4(color, 1.0);
 
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
--- src/shaders/earth/vertex.glsl
+++ src/shaders/earth/vertex.glsl
@@ -1,3 +1,4 @@
+varying vec3 vPosition;
 varying vec3 vNormal;
 varying vec2 vUv;
 
@@ -12,4 +13,5 @@ void main() {
 
     vUv = uv;
     vNormal = modelNormal.xyz;
+    vPosition = modelPosition.xyz;
 }
