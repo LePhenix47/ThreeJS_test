df378cf4a50a9a6360c72a6fb4f2ae094b976cea
Younes Lahouiti <loller763@gmail.com>
2026-09-18 13:40:11 +0200

    Added the tone mapping and color space to the frag shaders

--- src/shaders/earth/fragment.glsl
+++ src/shaders/earth/fragment.glsl
@@ -4,4 +4,7 @@ varying vec2 vUv;
 void main() {
     // gl_FragColor = vec4(vNormal, 1.0);
     gl_FragColor = vec4(vec3(vUv, 1.0), 1.0);
+
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
 }
