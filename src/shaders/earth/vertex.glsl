9f7e88427c4f06c6de65a85b67516adb49194304
Younes Lahouiti <loller763@gmail.com>
2026-09-18 13:29:58 +0200

    Fixed GLSL type mismatches

--- src/shaders/earth/vertex.glsl
+++ src/shaders/earth/vertex.glsl
@@ -2,14 +2,14 @@ varying vec3 vNormal;
 varying vec2 vUv;
 
 void main() {
-    vec3 modelPosition = modelMatrix * vec4(position, 1.0);
-    vec3 viewPosition = viewMatrix * modelPosition;
-    vec3 projectedPosition = projectionMatrix * viewPosition;
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
 
     gl_Position = projectedPosition;
 
-    vec3 modelNormal = modelMatrix * vec4(normal, 0.0);
+    vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
 
     vUv = uv;
-    vNormal = modelNormal;
+    vNormal = modelNormal.xyz;
 }
