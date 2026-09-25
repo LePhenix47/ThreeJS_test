aacb38cdeedf68a5e8965aa70eadc8ba7c597dc6
Younes Lahouiti <loller763@gmail.com>
2026-09-25 14:08:09 +0200

    Added logic to the type safe .env to strip prefixes on the keys

--- src/main.tsx
+++ src/main.tsx
@@ -12,7 +12,7 @@ import { routeTree } from "./routeTree.gen";
 // Create a new router instance
 const router = createRouter({
   routeTree,
-  basepath: env.VITE_BASE_PATH,
+  basepath: env.BASE_PATH,
 });
 
 // Register the router instance for type safety
